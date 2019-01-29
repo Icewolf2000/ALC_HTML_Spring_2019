4242e2c490f1e02fa44a818aa490da935c68bcb3
Unknown <cydney.n191@stu.nebo.edu>
2019-01-29 10:29:12 -0700

    Tried to Add Prompt for Taunting the Guard.

--- Choose Your Own Adventure Game/game.js
+++ Choose Your Own Adventure Game/game.js
@@ -15,14 +15,14 @@ function Game() {
     Prison();
     
     function Prison() {
-        var prison = prompt("You wake up, head pounding. After pacing for a bit, your vision clears. It appears you are a prison, though the reason why is unclear to you. /n - look around /n - go back to sleep /n - taunt the guard").toLowerCase();
+        var prison = prompt("You wake up, head pounding. After pacing for a bit, your vision clears. It appears you are a prison, though the reason why is unclear to you. What will you do? /n - look around /n - go back to sleep /n - taunt the guard").toLowerCase();
         
         if (prison == "look around" || prison == "look") {
-            var prisonLook = prompt("The cell is small, dirty, and damp. A rat scurries into a small hole in the corner. There is a barred window to the back, the bars so close together that there might have not been a window at all. There is an bed occupied by a snoring stranger to your right. The locked door to the front is being guarded by an armored soldier. The empty bed you woke up in is to your left. The corner of the room has a lumpy, moldy rug.");
+            var prisonLook = prompt("The cell is small, dirty, and damp. A rat scurries into a small hole in the corner. There is a barred window to the back, the bars so close together that there might have not been a window at all. There is an bed occupied by a snoring stranger to your right. The locked door to the front is being guarded by an armored lacertillian soldier. The lizard-like creature is at least a foot taller than you, and is sharpening a large axe. The empty bed you woke up in is to your left. The corner of the room has a lumpy, moldy rug.");
         }
         
         else if (prison == "go back to sleep" || prison == "sleep") {
-            alert("You fall back to sleep in your dirty cot. You dream of the outside world and long forgottem memories.");
+            alert("You manage fall back to sleep in your dirty cot. You dream of the outside world and long forgottem memories.");
             var resume = confirm("Do you wish to continue?");
             
             if (resume) {
@@ -33,6 +33,9 @@ function Game() {
             }
             
         }
+        
+        
+        
     }
     
     
